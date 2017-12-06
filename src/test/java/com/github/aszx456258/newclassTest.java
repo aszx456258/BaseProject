de99e36a1efe8b29e9b39b6108464879fbd20d4d
PC234 <ask5890111@yahoo.com.tw>
2017-12-06 12:34:46 +0800

    改

--- src/test/java/com/github/aszx456258/newclassTest.java
+++ src/test/java/com/github/aszx456258/newclassTest.java
@@ -3,7 +3,7 @@ package com.github.aszx456258;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+//check
 public class newclassTest {
     @Test
     public void prime() throws Exception {
