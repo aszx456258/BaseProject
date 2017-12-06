fdd62e90dfe74de923a111b4aa194c2991220211
PC234 <ask5890111@yahoo.com.tw>
2017-12-06 12:07:26 +0800

    123

---
+++ src/main/java/com/github/aszx456258/newclass.java
@@ -0,0 +1,25 @@
+package com.github.aszx456258;
+
+import java.util.Scanner;
+
+public class newclass{
+    public static void main(String[] args) {
+        System.out.println(prime(5));
+    }
+    public static int prime(int num)//檢查質數 1為質數 0則否
+    {
+        if(num == 1)
+        {
+            return 1;
+        }
+        for(int i = 2 ; i < num ; i++)
+        {
+            if(num%i == 0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+}
---
+++ src/test/java/com/github/aszx456258/newclassTest.java
@@ -0,0 +1,12 @@
+package com.github.aszx456258;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class newclassTest {
+    @Test
+    public void prime() throws Exception {
+    }
+
+}
